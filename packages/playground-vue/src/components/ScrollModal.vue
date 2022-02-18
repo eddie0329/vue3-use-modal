3afac1db00b7e6935566018733560858a4b501ff
eddie0329 <fdsa0329@naver.com>
2022-02-18 23:36:01 +0900

    :sparkles: Implement scroll behavior example

---
+++ packages/playground-vue/src/components/ScrollModal.vue
@@ -0,0 +1,33 @@
+<template>
+  <modal-wrapper @clickOutside="$emit('close')">
+    <div class="content">
+      <h1>Hello scroll Modal</h1>
+      <div class="block"></div>
+    </div>
+  </modal-wrapper>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ModalWrapper } from "use-modal";
+
+export default defineComponent({
+  components: { ModalWrapper },
+});
+</script>
+
+<style scoped>
+.content {
+  background-color: white;
+  border-radius: 10px;
+  width: 300px;
+  height: 300px;
+  overflow: auto;
+}
+
+.block {
+  width: 400px;
+  height: 400px;
+  background-color: yellow;
+}
+</style>
--- packages/playground-vue/src/views/HelloHome.vue
+++ packages/playground-vue/src/views/HelloHome.vue
@@ -1,22 +1,36 @@
 <template>
   <div class="home">
-    <button @click="onClick">CLICK ME! BUTTON</button>
+    <div>
+      <button @click="onClickSimpleModal">CLICK ME NORMAL MODAL</button>
+    </div>
+    <div>
+      <button @click="onClickScrollModal">CLICK ME SCROLL MODAL</button>
+    </div>
     <div class="block"></div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { useModal } from 'use-modal';
-import { ref } from 'vue';
-import SimpleModal from '../components/SimpleModal.vue';
+import { useModal } from "use-modal";
+import { ref } from "vue";
+import SimpleModal from "../components/SimpleModal.vue";
+import ScrollModal from "../components/ScrollModal.vue";
 
 const modal = useModal();
 
-const myName = ref<string>('eddie');
+const myName = ref<string>("eddie");
 
-const onClick = async () => {
-  const name = await modal.addModal({ key: 'SimpleModal', component: SimpleModal, options: { myName } });
+const onClickSimpleModal = async () => {
+  const name = await modal.addModal({
+    key: "SimpleModal",
+    component: SimpleModal,
+    options: { myName },
+  });
   alert(name);
+};
+
+const onClickScrollModal = async () => {
+  await modal.addModal({ key: "ScrollModal", component: ScrollModal });
 };
 </script>
 
