31ba304e33efce9665bc64979e8f3e74cf737bec
eddie0329 <fdsa0329@naver.com>
2022-03-05 21:57:56 +0900

    :sparkles: Implement add modal

--- src/ModalPlugin.ts
+++ src/ModalPlugin.ts
@@ -1,13 +1,23 @@
 import {createApp} from 'vue';
 import ModalContainer from './components/ModalContainer.vue';
 import ModalProxy from './ModalProxy';
+import {err} from './utils';
 import type {ModalExposed} from './types/modal-proxy';
+
+const createModalEl = () => {
+  if (!document) err('No document.');
+  const modalEl = document.createElement('div');
+  modalEl.setAttribute('id', 'modal');
+  document.body.prepend(modalEl);
+  return modalEl;
+};
 
 export default {
   install(app) {
+    const modalEl = createModalEl();
     const modalContainer = createApp(ModalContainer);
     const modalProxy = ModalProxy.getInstance();
-    modalProxy.setModalExposed(modalContainer.mount('#modal') as ModalExposed);
+    modalProxy.setModalExposed(modalContainer.mount(modalEl) as ModalExposed);
     app.config.globalProperties.$modal = modalProxy;
     modalContainer.config.globalProperties = { ...modalContainer.config.globalProperties, ...app.config.globalProperties};
   }
