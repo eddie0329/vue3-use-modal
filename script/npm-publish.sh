2b0aff3121cd9f28c5d393f85eace94a639a7ca7
eddie0329 <fdsa0329@naver.com>
2022-03-07 14:17:26 +0900

    :sparkles: Implement script

---
+++ script/npm-publish.sh
@@ -0,0 +1,11 @@
+echo "Current version:" $(grep version ../package.json | sed -E 's/^.*"([0-9][^"]+)".*$/\1/')
+
+read -p "Releasing v$VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  npm version patch
+  echo "Releasing version:" $(grep version ../package.json | sed -E 's/^.*"([0-9][^"]+)".*$/\1/')
+  git push origin v(grep version ../package.json)
+fi
