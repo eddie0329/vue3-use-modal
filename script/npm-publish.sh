212d59a1fba57dbe0902bf5af86d76e466ce2173
eddie0329 <fdsa0329@naver.com>
2022-03-07 14:22:50 +0900

    :sparkles: Change script

--- script/npm-publish.sh
+++ script/npm-publish.sh
@@ -1,11 +1,11 @@
-echo "Current version:" $(grep version ../package.json | sed -E 's/^.*"([0-9][^"]+)".*$/\1/')
+echo "Current version:" $(grep version package.json | sed -E 's/^.*"([0-9][^"]+)".*$/\1/')
 
-read -p "Releasing v$VERSION - are you sure? (y/n)" -n 1 -r
+read -p "Are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   npm version patch
-  echo "Releasing version:" $(grep version ../package.json | sed -E 's/^.*"([0-9][^"]+)".*$/\1/')
-  git push origin v$(grep version ../package.json | sed -E 's/^.*"([0-9][^"]+)".*$/\1/')
+  echo "Releasing version:" $(grep version package.json | sed -E 's/^.*"([0-9][^"]+)".*$/\1/')
+  git push origin v$(grep version package.json | sed -E 's/^.*"([0-9][^"]+)".*$/\1/')
 fi
